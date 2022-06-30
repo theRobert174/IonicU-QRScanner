89ab5261d1a5c0c3c89ce969752bb7e9deeb5365
Rodrigo Mtz <88547726+theRobert174@users.noreply.github.com>
2022-06-30 12:41:34 -0500

    model y service

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner/ngx';
+import { DataLocalService } from '../../services/data-local.service';
 
 @Component({
   selector: 'app-tab1',
@@ -13,7 +14,7 @@ export class Tab1Page {
     allowSlideNext: false
   }
 
-  constructor(private barcodeScanner: BarcodeScanner) {}
+  constructor(private barcodeScanner: BarcodeScanner, private dataLocal: DataLocalService) {}
 
   ionViewDidEnter(){
     console.log('DidEnter');
@@ -38,8 +39,12 @@ export class Tab1Page {
   scan(){
     this.barcodeScanner.scan().then(barcodeData => {
       console.log('Barcode data', barcodeData);
+      if(!barcodeData.cancelled){
+        this.dataLocal.guardarRegistro(barcodeData.format, barcodeData.text);
+      }
      }).catch(err => {
          console.log('Error', err);
+         this.dataLocal.guardarRegistro('QRcode', "https://fernando-herrera.com");
      });
   }
 
